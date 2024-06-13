1f6df7881a4c7b1ea5a94aa42bc156a3822726d8
harshxraj <harshraj0588@gmail.com>
2024-06-13 12:22:58 +0530

    controllers  created

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { getProgress, updateProgress } = require("../controllers/progressController");
+const { getUserProgress, updateProgress } = require("../controllers/progressController");
 const authenticateToken = require("../middlewares/authMiddleware");
 const authorizeRole = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
 
-router.get("/users/:id/progress", authenticateToken, authorizeRole(["STUDENT", "TEACHER"]), getProgress);
+router.get("/users/:id/progress", authenticateToken, authorizeRole(["STUDENT", "TEACHER"]), getUserProgress);
 router.post("/users/:id/progress", authenticateToken, authorizeRole(["TEACHER"]), updateProgress);
 
 module.exports = router;
